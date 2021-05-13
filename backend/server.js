37ac5e5f58af12b9daee853fcf732e644ed7a922
ismaiel <ismaiel.nab@gmail.com>
2021-05-13 08:12:32 +0200

    fix heroku

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import path from 'path'
+import morgan from 'morgan'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import expressionsRoutes from './routes/expressionsRoutes.js'
@@ -10,10 +12,29 @@ connectDB()
 
 const app = express()
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
 app.use(express.json())
 
 app.use('/api/expression/', expressionsRoutes)
 app.use('/api/user', userRoutes)
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 
 app.use(notFound)
 app.use(errorHandler)
