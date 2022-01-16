b9ea5d07071613f1389c6aed63ab307f028c56b7
Ismaiel <ismaiel.nab@gmail.com>
2022-01-16 14:17:09 +0100

    new update

--- frontend/src/components/Exercises.js
+++ frontend/src/components/Exercises.js
@@ -7,14 +7,14 @@ const Exercises = ({ id, expression, inState }) => {
   const [valueTyped] = useAnswer(id)
 
   return (
-    <Row className='my-3 justify-content-between mx-auto px-2 align-items-center' key={id}>
-      <Col md={4} xs={10} className='rounded py-2 my-auto sbq'>
+    <Row className='my-3 mx-auto align-items-center' key={id}>
+      <Col md={4} xs={12} className='rounded py-2 my-auto sbq'>
         <strong>{expression}</strong>
       </Col>
-      <Col lg={2} md={2} xs={10} className='text-center py-2 my-auto'>
+      <Col lg={2} md={2} xs={12} className='text-center py-2 my-auto'>
         <h2>=</h2>
       </Col>
-      <Col md={6} xs={10} className='sba'>
+      <Col md={6} xs={12} className='sba'>
         <input onChange={valueTyped} value={inState[id]} className='inp' />
       </Col>
     </Row>
--- frontend/src/components/Play.js
+++ frontend/src/components/Play.js
@@ -7,127 +7,128 @@ import fetchQuestions from '../Utils/fetchQuestions'
 import Exercises from './Exercises'
 import Loader from './Loader'
 
-
 const Play = ({ history }) => {
-    const { total, success, fail, setFlag, setFail, } = useContext(PlayCountContext)
+  const { total, success, fail, setFlag, setFail } =
+    useContext(PlayCountContext)
 
-    const {
-        first,
-        setFirst,
-        second,
-        setSecond,
-        third,
-        setThird,
-        fourth,
-        setFourth,
-        fifth,
-        setFifth,
-        loading,
-        setLoading,
-        expressions,
-        setExpressions,
-        user,
-        setUser,
-    } = useContext(GlobalContext)
+  const {
+    first,
+    setFirst,
+    second,
+    setSecond,
+    third,
+    setThird,
+    fourth,
+    setFourth,
+    fifth,
+    setFifth,
+    loading,
+    setLoading,
+    expressions,
+    setExpressions,
+    user,
+    setUser,
+  } = useContext(GlobalContext)
 
-    const inState = [first, second, third, fourth, fifth]
+  const inState = [first, second, third, fourth, fifth]
 
-    localStorage.setItem('answers', JSON.stringify(inState))
+  localStorage.setItem('answers', JSON.stringify(inState))
 
-    const playInfo = [total, success, fail]
+  const playInfo = [total, success, fail]
 
-    localStorage.setItem('playHistory', JSON.stringify(playInfo))
+  localStorage.setItem('playHistory', JSON.stringify(playInfo))
 
-if(fail >2){
+  if (fail > 2) {
     history.push('./StopPlay')
-}
+  }
 
-    if (!user) {
-        const userInfo = localStorage.getItem('userInfo')
-            ? JSON.parse(localStorage.getItem('userInfo'))
-            : null
-        setUser(userInfo)
-        history.push('/')
+  if (!user) {
+    const userInfo = localStorage.getItem('userInfo')
+      ? JSON.parse(localStorage.getItem('userInfo'))
+      : null
+    setUser(userInfo)
+    history.push('/')
+  }
+
+  const getQuestions = async () => {
+    const q = await fetchQuestions()
+    setExpressions(q)
+    setFirst('')
+    setSecond('')
+    setThird('')
+    setFourth('')
+    setFifth('')
+    setFail(0)
+  }
+
+  const confirmHandler = (e) => {
+    e.preventDefault()
+    setLoading(true)
+    if (
+      first === expressions[0].answer &&
+      second === expressions[1].answer &&
+      third === expressions[2].answer &&
+      fourth === expressions[3].answer &&
+      fifth === expressions[4].answer
+    ) {
+      setFlag(true)
+    } else {
+      setFlag(false)
     }
+    setLoading(false)
+    history.push('/result')
+  }
 
-    const getQuestions = async () => {
-        const q = await fetchQuestions()
-        setExpressions(q)
-        setFirst('')
-        setSecond('')
-        setThird('')
-        setFourth('')
-        setFifth('')
-        setFail(0)
-    }
-
-    const confirmHandler = (e) => {
-        e.preventDefault()
-        setLoading(true)
-        if (
-            first === expressions[0].answer &&
-            second === expressions[1].answer &&
-            third === expressions[2].answer &&
-            fourth === expressions[3].answer &&
-            fifth === expressions[4].answer
-        ) {
-            setFlag(true)
-        } else {
-            setFlag(false)
-        }
-        setLoading(false)
-        history.push('/result')
-    }
-
-
-    return (
+  return (
+    <Container>
+      {user && <Row className='p-1 text-info'>Welcome {user.name}</Row>}
+      {/* {playCount > 2 && <StopPlay />} */}
+      {loading && <Loader />}
+      {user && (
         <Container>
-            {user &&
-                <Row className='p-1 text-info'>
-                    Welcome {user.name}
-                </Row>}
-            {/* {playCount > 2 && <StopPlay />} */}
-            {loading && <Loader />}
-            {user && (
-                <Container>
-                    <Row>                        
-                        <Col align='center'>
-                            <Button
-                                variant='info'
-                                className='sbg'
-                                onClick={getQuestions}
-                            >Get New Questions
-                            </Button>
-                        </Col>
-                    </Row>
-                    {expressions.length !== 0 && (
-                        <>
-                            <Row className='my-3 justify-content-between mx-auto text-info'>
-                                <Col as='h4' md={4} xs={5} className='text-center'>Questions</Col>
-                                <Col lg={2} md={2} xs={1}></Col>
-                                <Col as='h4' md={6} xs={5} className='text-center'>Answers</Col>
-                            </Row>
-                            {expressions.map((item) => (
-                                <Exercises
-                                    key={item._id}
-                                    id={expressions.indexOf(item)}
-                                    expression={item.question}
-                                    inState={inState}
-                                />
-                            ))}
-                            <Row className='px-3 justify-content-center'>
-                                <Col className='text-center'>
-                                <Button type='submit'
-                                    md={4}
-                                    onClick={confirmHandler}
-                                    className='sbg mb-5'>
-                                    <strong className='text-info'>Check The Result</strong></Button>
-                                </Col>
-                            </Row>
-                        </>
-                    )}
-                </Container>
-            )}
-        </Container>)
+          <Row>
+            <Col align='center'>
+              <Button variant='info' className='sbg' onClick={getQuestions}>
+                Get New Questions
+              </Button>
+            </Col>
+          </Row>
+          {expressions.length !== 0 && (
+            <>
+              <Row className='my-3 mx-auto text-info'>
+                <Col as='h4' md={4} xs={12} className='text-center'>
+                  Questions
+                </Col>
+                <Col lg={2} md={2} xs={12}></Col>
+                <Col as='h4' md={6} xs={12} className='text-center'>
+                  Answers
+                </Col>
+              </Row>
+              {expressions.map((item) => (
+                <Exercises
+                  key={item._id}
+                  id={expressions.indexOf(item)}
+                  expression={item.question}
+                  inState={inState}
+                />
+              ))}
+              <Row className='px-3'>
+                <Col className='text-center'>
+                  <Button
+                    type='submit'
+                    md={4}
+                    onClick={confirmHandler}
+                    className='sbg mb-5'
+                  >
+                    <strong className='text-info'>Check The Result</strong>
+                  </Button>
+                </Col>
+              </Row>
+            </>
+          )}
+        </Container>
+      )}
+    </Container>
+  )
 }
 export default Play
